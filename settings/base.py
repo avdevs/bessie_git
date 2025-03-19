ef641f580e1aa63d3ddec880c6ac8b9d38695e4f
Jakub Gawlik <info@caasi.dev>
2025-03-19 21:54:01 +0000

    sort out static files

--- settings/base.py
+++ settings/base.py
@@ -149,7 +149,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -160,4 +160,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/dashboard"
 LOGOUT_REDIRECT_URL = "/"
-
