d21bcaa3d7b81938ce9ff429d54f06f4de5a4a97
Jakub Gawlik <info@caasi.dev>
2025-04-04 08:15:18 +0100

    add summary models

--- bessie/models.py
+++ bessie/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from users.models import User
 from django.core.exceptions import ValidationError
 
+RISK_LEVELS = [
+    ("low", "Low"),
+    ("medium", "Medium"),
+    ("high", "High"),
+    ("veryhigh", "Very High"),
+]
+
 
 class Company(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -60,6 +67,41 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CompanyRiskSummary(models.Model):
+    company = models.OneToOneField(Company, on_delete=models.CASCADE)
+    stress_and_wellbeing_risk_level = models.CharField(
+        choices=RISK_LEVELS,
+        default="low",
+    )
+    stress_and_wellbeing_in_place = models.TextField()
+    stress_and_wellbeing_recommendations = models.TextField()
+    stress_and_wellbeing_risk_date = models.DateField()
+    workplace_stress_risk_level = models.CharField(
+        choices=RISK_LEVELS,
+        default="low",
+    )
+    workplace_stress_in_place = models.TextField()
+    workplace_stress_recommendations = models.TextField()
+    workplace_stress_risk_date = models.DateField()
+    presenteeism_risk_level = models.CharField(
+        choices=RISK_LEVELS,
+        default="low",
+    )
+    presenteeism_in_place = models.TextField()
+    presenteeism_recommendations = models.TextField()
+    presenteeism_risk_date = models.DateField()
+    wider_risks_risk_level = models.CharField(
+        choices=RISK_LEVELS,
+        default="low",
+    )
+    wider_risks_in_place = models.TextField()
+    wider_risks_recommendations = models.TextField()
+    wider_risks_risk_date = models.DateField()
+
+    def __str__(self):
+        return f"Risk Summary for {self.company.name}"
 
 
 class CompanyAdmin(models.Model):
