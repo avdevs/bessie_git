31443c393878d2cea47133703d84be0dfba7fb4b
Jakub Gawlik <info@caasi.dev>
2025-06-03 22:57:18 +0100

    expose employee unique id

--- bessie/admin.py
+++ bessie/admin.py
@@ -6,77 +6,82 @@ from .models import *
 
 @admin.register(BessieResponse)
 class BessieResponseAdmin(admin.ModelAdmin):
-  list_display = [
-      "id",
-      "get_employee_first_name",
-      "get_employee_last_name",
-      "employee__company__name",
-  ]
-  list_filter = ["employee__company__name"]
+	list_display = [
+		"id",
+		"get_employee_first_name",
+		"get_employee_last_name",
+		"employee__company__name",
+	]
+	list_filter = ["employee__company__name"]
 
-  def get_employee_first_name(self, obj):
-    return obj.employee.user.first_name
+	def get_employee_first_name(self, obj):
+		return obj.employee.user.first_name
 
-  get_employee_first_name.short_description = "Employee First Name"
+	get_employee_first_name.short_description = "Employee First Name"
 
-  def get_employee_last_name(self, obj):
-    return obj.employee.user.last_name
+	def get_employee_last_name(self, obj):
+		return obj.employee.user.last_name
 
-  get_employee_last_name.short_description = "Employee Last Name"
+	get_employee_last_name.short_description = "Employee Last Name"
 
 
 @admin.register(BessieResult)
 class BessieResultAdmin(admin.ModelAdmin):
-  list_display = [
-      "id",
-      "get_employee_first_name",
-      "get_employee_last_name",
-      "company__name",
-  ]
-  list_filter = ["company__name"]
+	list_display = [
+		"id",
+		"get_employee_first_name",
+		"get_employee_last_name",
+		"company__name",
+	]
+	list_filter = ["company__name"]
 
-  def get_employee_first_name(self, obj):
-    return obj.response.employee.user.first_name
+	def get_employee_first_name(self, obj):
+		return obj.response.employee.user.first_name
 
-  get_employee_first_name.short_description = "Employee First Name"
+	get_employee_first_name.short_description = "Employee First Name"
 
-  def get_employee_last_name(self, obj):
-    return obj.response.employee.user.last_name
+	def get_employee_last_name(self, obj):
+		return obj.response.employee.user.last_name
 
-  get_employee_last_name.short_description = "Employee Last Name"
+	get_employee_last_name.short_description = "Employee Last Name"
 
 
 @admin.register(CompanyAdmin)
 class CompanyAdminAdmin(admin.ModelAdmin):
-  list_display = ["user__first_name", "user__last_name", "company__name"]
-  list_filter = ["company__name"]
+	list_display = ["user__first_name", "user__last_name", "company__name"]
+	list_filter = ["company__name"]
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-  list_display = [
-      "name",
-      "slots",
-      "survey_start_date",
-      "survey_completion_date",
-      "strategy_meeting_date",
-  ]
-  search_fields = ["name"]
+	list_display = [
+		"name",
+		"slots",
+		"survey_start_date",
+		"survey_completion_date",
+		"strategy_meeting_date",
+	]
+	search_fields = ["name"]
 
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-  list_display = ["user__first_name",
-                  "user__last_name", "company__name", "team"]
-  list_filter = ["company__name"]
-  search_fields = ["user__first_name", "user__last_name"]
+	list_display = [
+		"user__first_name",
+		"user__last_name",
+		"company__name",
+		"unique_id",
+		"team",
+	]
+	list_filter = ["company__name"]
+	search_fields = ["user__first_name", "user__last_name"]
 
 
 # register CompanyRiskSummary model
 @admin.register(CompanyRiskSummary)
 class CompanyRiskSummaryAdmin(admin.ModelAdmin):
-  list_filter = ["company__name"]
-  search_fields = ["company__name"]
+	list_filter = ["company__name"]
+	search_fields = ["company__name"]
 
 
 admin.site.register(EmployeeProcessTask)
