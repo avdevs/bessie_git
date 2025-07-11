6f9b3bef144c72f4dcf1c6c8b2d5bb8601d0410f
Jakub Gawlik <info@caasi.dev>
2025-07-11 10:36:21 +0100

    expose is bessie admin field in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,61 +1,72 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 
 # Register your models here.
 class UserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = User
-    list_display = (
-        "email",
-        "first_name",
-        "last_name",
-        "user_type",
-        "is_staff",
-        "is_active",
-    )
-    list_filter = (
-        "email",
-        "first_name",
-        "last_name",
-        "user_type",
-        "is_staff",
-        "is_active",
-    )
-    fieldsets = (
-        (None, {"fields": (
-            "first_name",
-            "last_name", 
-            "email",
-            "user_type", 
-            "password")}
-        ),
-        ("Permissions", {"fields": (
-            "is_staff", 
-            "is_active", 
-            "groups", 
-            "user_permissions")}
-        ),
-    )
-    add_fieldsets = (
-        ( None, {"fields": (
-            "first_name",
-            "last_name",
-            "email",
-            "user_type",
-            "password1",
-            "password2",
-            "is_staff",
-            "is_active",
-            "groups",
-            "user_permissions")}
-        ),
-    )
-    search_fields = ("email",)
-    ordering = ("email",)
+	add_form = CustomUserCreationForm
+	form = CustomUserChangeForm
+	model = User
+	list_display = (
+		"email",
+		"first_name",
+		"last_name",
+		"user_type",
+		"bessie_admin",
+		"is_staff",
+		"is_active",
+	)
+	list_filter = (
+		"email",
+		"first_name",
+		"last_name",
+		"user_type",
+		"is_staff",
+		"is_active",
+	)
+	fieldsets = (
+		(
+			None,
+			{
+				"fields": (
+					"first_name",
+					"last_name",
+					"email",
+					"user_type",
+					"password",
+					"bessie_admin",
+				)
+			},
+		),
+		(
+			"Permissions",
+			{"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+		),
+	)
+	add_fieldsets = (
+		(
+			None,
+			{
+				"fields": (
+					"first_name",
+					"last_name",
+					"email",
+					"user_type",
+					"password1",
+					"password2",
+					"is_staff",
+					"is_active",
+					"groups",
+					"user_permissions",
+				)
+			},
+		),
+	)
+	search_fields = ("email",)
+	ordering = ("email",)
 
 
 admin.site.register(User, UserAdmin)
