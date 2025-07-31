7db1d620d39153798052518914c2f4f54a51ad3f
Liam <liam@bitjam.org.uk>
2025-07-31 07:52:28 +0100

    Revert "merge pt 2"
    This reverts commit ab1b1cd1eb609e2189067ced56001212e9dd87a4.

--- settings/base.py
+++ settings/base.py
@@ -1,7 +1,6 @@
 import os
-from os.path import dirname, join
 from pathlib import Path
-
+from os.path import join, dirname
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -13,12 +12,7 @@ SECRET_KEY = "django-insecure-c4a^r3!(iqpr=^4aakno!l71d*5$x#)gc8vo2iqh#$5evg$9vg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-	"127.0.0.1",
-	"localhost",
-	"bessiestressriskassessment.com",
-	"bessie-staging.com",
-]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "bessiestressriskassessment.com"]
 
 AUTH_USER_MODEL = "users.User"
 
@@ -37,28 +31,25 @@ EMAILPORT = os.getenv("EMAILPORT")
 EMAILUSER = os.getenv("EMAILUSER")
 EMAILPASSWORD = os.getenv("EMAILPASSWORD")
 
-# Site domain settings for email URLs
-SITE_DOMAIN = os.getenv("SITE_DOMAIN", "http://localhost:8000")
-
 # Database... If postgresql envs are not set, use sqlite3
 if DBHOST and DBPORT and DBNAME and DBUSER and DBPASS:
-	DATABASES = {
-		"default": {
-			"ENGINE": "django.db.backends.postgresql",
-			"NAME": DBNAME,
-			"USER": DBUSER,
-			"PASSWORD": DBPASS,
-			"HOST": DBHOST,
-			"PORT": DBPORT,
-		}
-	}
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": DBNAME,
+            "USER": DBUSER,
+            "PASSWORD": DBPASS,
+            "HOST": DBHOST,
+            "PORT": DBPORT,
+        }
+    }
 else:
-	DATABASES = {
-		"default": {
-			"ENGINE": "django.db.backends.sqlite3",
-			"NAME": BASE_DIR / "db.sqlite3",
-		}
-	}
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -66,53 +57,52 @@ EMAIL_HOST = EMAILHOST
 EMAIL_PORT = EMAILPORT
 EMAIL_HOST_USER = EMAILUSER
 EMAIL_HOST_PASSWORD = EMAILPASSWORD
-DEFAULT_FROM_EMAIL = "No-Reply | Bessie Portal"
+DEFAULT_FROM_EMAIL = "no-reply@bessiestressriskassessment.com"
 SERVER_EMAIL = "no-reply@bessiestressriskassessment.com"
 
 
 # Application definition
 INSTALLED_APPS = [
-	"django.contrib.admin",
-	"django.contrib.auth",
-	"django.contrib.contenttypes",
-	"django.contrib.sessions",
-	"django.contrib.messages",
-	"django.contrib.staticfiles",
-	"background_task",
-	"users",
-	"mini_bessie",
-	"bessie",
-	"pages",
-	"formtools",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "users",
+    "mini_bessie",
+    "bessie",
+    "pages",
+    "formtools",
 ]
 
 MIDDLEWARE = [
-	"django.middleware.security.SecurityMiddleware",
-	"whitenoise.middleware.WhiteNoiseMiddleware",
-	"django.contrib.sessions.middleware.SessionMiddleware",
-	"django.middleware.common.CommonMiddleware",
-	"django.middleware.csrf.CsrfViewMiddleware",
-	"django.contrib.auth.middleware.AuthenticationMiddleware",
-	"django.contrib.messages.middleware.MessageMiddleware",
-	"django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "bessie_tech.urls"
 
 TEMPLATES = [
-	{
-		"BACKEND": "django.template.backends.django.DjangoTemplates",
-		"DIRS": [BASE_DIR / "templates"],
-		"APP_DIRS": True,
-		"OPTIONS": {
-			"context_processors": [
-				"django.template.context_processors.debug",
-				"django.template.context_processors.request",
-				"django.contrib.auth.context_processors.auth",
-				"django.contrib.messages.context_processors.messages",
-			],
-		},
-	},
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
 ]
 
 # LOGGING = {
@@ -138,12 +128,12 @@ WSGI_APPLICATION = "bessie_tech.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-	},
-	{
-		"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-	},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
 ]
 
 
